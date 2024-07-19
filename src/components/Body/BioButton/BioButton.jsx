799afb149ff7a3f82f3bfd3e2d2f986492cbe3d9
d-lee-te <dlee.ggwp@gmail.com>
2024-07-19 02:13:35 -0400

    v1.12 - OCD-esqe proofreading

--- src/components/Body/BioButton/BioButton.jsx
+++ src/components/Body/BioButton/BioButton.jsx
@@ -48,7 +48,7 @@ const BioButton = ({ spinTime }) => {
             <text>
             I’m David! <br/><br/>
             I’m a CS(Intel/Devices) graduate from Georgia Tech seeking full-time positions in software engineering and related with aspirations of becoming a full-stack developer.<br/><br/>
-            I have a moderate amount of experience with Python with additional light background experience in C, Java, ASM, and others. I’ve made my own Neural Net, and as an extension of that, I've learned or looked into: AI Principles, Deep Learning, and Computer Vision principles.Currently, I am reviewing material from my studies and building projects with new languages.<br/><br/>
+            I have a moderate amount of experience with Python with additional light background experience in C, Java, ASM, and others. I’ve made my own Neural Net, and as an extension of that, I've learned or looked into: AI Principles, Deep Learning, and Computer Vision principles. Currently, I am reviewing material from my studies and building projects with new languages.<br/><br/>
             In my free time, I like to brainstorm and dissect games into what makes them enjoyable, listen to music, follow esports loosely (Previously an Esports President in both high school and college), and learn new skills in any focus as I enjoy learning new things!<br/><br/>
             Excited to meet you!
 
