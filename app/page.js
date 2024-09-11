4447f8051fe2de16db9f0d71487162f9b2541521
d-lee-te <dlee.ggwp@gmail.com>
2024-09-11 13:32:41 -0400

    margin details

--- app/page.js
+++ app/page.js
@@ -7,15 +7,19 @@ export default function Home() {
     <body>
       <ParticleBackground className="fixed inset-0 w-full h-full -z-0"/>
       <main className="relative">
-      
-      <div className="flex justify-center items-center h-screen -z-10">
-        <div className='flex flex-col gap-3'>
-          <Header className/>
-          <Navi/>
+        <div className="flex justify-center h-screen -z-10">
+          <div className='flex flex-col gap-4'>
+            <div className='mt-20'>
+              <Header/>
+            </div>
+            <hr className='border-2 w-3/6 rounded-lg mx-auto'/>
+            <div className=''>
+              <Navi/>
+              <hr className='border-2 w-2/6 rounded-lg mx-auto opacity-15 mt-32'/>
+            </div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
     </body>
-    
   );
 }
